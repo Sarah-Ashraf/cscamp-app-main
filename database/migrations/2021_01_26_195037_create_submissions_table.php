9c4a76c737cb088ad64e2c4d1ffabaf0694b6764
SARAHASHRAF\hp 1015 <sarahashaher0@gmail.com>
2021-01-27 00:15:00 +0200

    first commit

---
+++ database/migrations/2021_01_26_195037_create_submissions_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateSubmissionsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('submissions', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->foreign('events_id')->references('id')->on('events');
+            $table->string('email')->unique();
+            $table->string('phone');
+            $table->JSON('additional_data');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('_submissions');
+    }
+}
